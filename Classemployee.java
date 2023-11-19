816eed1e5d37f6147f3ee72807b8830efc7b7e75
Souhaïl Bechchari <146872818+SouhailBechchari@users.noreply.github.com>
2023-11-19 23:56:59 +0100

    Create Classemployee.java

---
+++ Classemployee.java
@@ -0,0 +1,19 @@
+public class Employee {
+
+    private String name;
+    private int baseSalary;
+    private int bonusHrs;
+    private int sales;
+
+
+    public Player(String name, int baseSalary, int bonusHrs , int sales) {
+        this.name = name;
+        this.baseSalary = baseSalary;
+        this.bonusHrs = bonusHrs;
+        this.sales = sales;
+    }
+
+    public void CalculateNetSalary() {
+        System.out.println("Voici Le Netsalary")
+    }
+
